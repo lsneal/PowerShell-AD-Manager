03ab96fb19b7ae7f9df495e029d9cdef611a4a9f
amnesia <post.xes42@passmail.net>
2025-03-02 17:21:22 +0100

    Create user

--- manage.ps1
+++ manage.ps1
@@ -1,5 +1,23 @@
 function CreateUser()
 {
+    $username = Read-Host "Enter username: "
+    $userpname = Read-Host "Enter principal username: "
+    $name = Read-Host "Enter name: "
+    $firstname = Read-Host "Enter first name: "
+    $lastname = Read-Host "Enter last name: "
+    $dname = Read-Host "Enter display name: "
+    $password = Read-Host "Enter password: "
+
+    New-ADuser  -SamAccountName "$username" `
+                -UserPrincipalName "$userpname" `
+                -Name "$name" `
+                -GivenName "$firstname" `
+                -Surname "$lastname" `
+                -DisplayName "$dname" `
+                -Enabled $true `
+                -PasswordNeverExpires $true `
+                -AccountPassword (ConvertTo-SecureString -AsPlainText "$password" -Force) `
+                -PassThru
     Write-Host "User created"
 }
 
